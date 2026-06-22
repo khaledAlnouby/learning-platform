a1288e3c8ac3e83f7fa3949e02c0f70adcfd2e34
khaledAlnouby <121772428+khaledAlnouby@users.noreply.github.com>
2026-06-22 20:54:34 +0300

    small enhancements

--- create-databases.sql
+++ create-databases.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE users_db;
-CREATE DATABASE courses_db;
-CREATE DATABASE enrollment_db;
-CREATE DATABASE notification_db;
+CREATE DATABASE users_db ;
+CREATE DATABASE courses_db ;
+CREATE DATABASE enrollment_db ;
+CREATE DATABASE notification_db ;
